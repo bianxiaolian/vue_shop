34e3a02b56a40cc86d6f84c272f4ff83b817634a
bianxiaolian <bianxiaolia@163.com>
2020-01-07 17:24:02 +0800

    login code

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from './../components/Login.vue'
+import Home from './../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/login' },
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home
   }
+
 ]
 
 const router = new VueRouter({
   routes
 })
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')//如果没有登录token就不存在，因此拦截用户访问其他页面，只显示login页面
+  next()
+})
+//next是一个放行函数
 
 export default router
